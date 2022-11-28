11a5492a594e80ab5804009b6c2a51c56fd2b801
GARCIA_STEPHANIE <45000steph@gmail.com>
2022-11-28 13:24:36 +0100

    main

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,34 @@
+<?php
+
+// src/Controller/ProgramController.php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\HttpFoundation\Response;
+
+
+class  DefaultController extends AbstractController
+
+{
+      /**
+
+     * @Route("/program/", name="program_index")
+
+     */
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+
+    {
+
+        return $this->render('program/index.html.twig', [
+
+            'website' => 'Wild Series',
+     
+         ]);
+        
+    }
+
+}
